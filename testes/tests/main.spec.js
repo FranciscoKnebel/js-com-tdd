38a607a16c445c3250a7d67094eb32321c1685af
FranciscoKnebel <franciscopaivaknebel@gmail.com>
2017-08-12 15:44:47 -0300

    Base de testes/

---
+++ testes/tests/main.spec.js
@@ -0,0 +1,11 @@
+const expect = require('chai').expect;
+
+describe('Main', () => {
+	describe('Method A', () => {
+		context('Case 1', () => {
+			it('should happen x', () => {
+
+			});
+		});
+	});
+});
